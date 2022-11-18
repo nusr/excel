3c8ffad997e5d9cdcfdff5b22635850c5cc5e32a
Steve Xu <1161176156@qq.com>
2022-11-18 17:26:39 +0800

    Update index.ts

--- src/react/index.ts
+++ src/react/index.ts
@@ -1,1 +1,15 @@
+import { init, VNode } from './back';
+import { htmlDomApi } from './dom'
 export { render, h } from './react';
+const patch = init(htmlDomApi);
+
+export function realRender(container: Element & { vDom?: VNode }, vNode: VNode): VNode {
+    let temp: VNode;
+    if (container.vDom) {
+        temp = patch(container.vDom as VNode, vNode);
+    } else {
+        temp = patch(patch(container, vNode), vNode);
+    }
+    container.vDom = temp;
+    return temp;
+}
