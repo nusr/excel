e22ef52f425afe80a1cc7561b9ef1e720c7a6346
Steve Xu <stevexugc@gmail.com>
2024-10-28 15:56:28 +0800

    feat: performance monitor

--- scripts/demo.tsx
+++ scripts/demo.tsx
@@ -2,6 +2,7 @@ import {
   init,
   browserTracingIntegration,
   replayIntegration,
+  browserProfilingIntegration,
 } from '@sentry/react';
 import { createRoot } from 'react-dom/client';
 import React, { StrictMode } from 'react';
@@ -34,14 +35,13 @@ if (location.hostname !== 'localhost') {
         maskAllText: false,
         blockAllMedia: false,
       }),
+      browserProfilingIntegration(),
     ],
-    // Performance Monitoring
-    tracesSampleRate: 1.0, //  Capture 100% of the transactions
-    // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+    tracesSampleRate: 1.0,
     tracePropagationTargets: ['nusr.github.io'],
-    // Session Replay
-    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
+    profilesSampleRate: 1.0,
   });
 }
 
