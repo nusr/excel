4eee070282ea54971a779236ffd90d9667aa3646
nusr <nusr@users.noreply.github.com>
2024-11-11 13:29:34 +0000

    deploy: 100daff85c1f8113609bf9bc7024b8d6e3459bc9

--- worker.js
+++ worker.js
@@ -22,5 +22,5 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
 */
-import{a as m}from"./chunk-COPFB74N.js";import"./chunk-2EW72QMQ.js";import{a as o}from"./chunk-PRJU7QU5.js";import"./chunk-6ACQG57Y.js";import"./chunk-AI5EXC2Q.js";o(m);
+import{a as m}from"./chunk-ZQ3I7DXV.js";import"./chunk-LKIDBLYH.js";import{a as o}from"./chunk-PRJU7QU5.js";import"./chunk-CRSUINH5.js";import"./chunk-AI5EXC2Q.js";o(m);
 //# sourceMappingURL=worker.js.map
