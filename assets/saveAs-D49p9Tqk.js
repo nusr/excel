f35ebf6dc23c22019cbb8a2bfc65786e59d43d85
nusr <nusr@users.noreply.github.com>
2024-11-17 05:41:47 +0000

    deploy: 9d7dde94c99707aece2c68bda9dad0abe81498b1

---
+++ assets/saveAs-D49p9Tqk.js
@@ -0,0 +1,2 @@
+function a(t,n){let e=document.createElement("a");e.download=n||"download",e.rel="noopener",e.target="_blank",e.setAttribute("data-testid","save-as-link"),typeof t=="string"?e.href=t:e.href=URL.createObjectURL(t),e.dispatchEvent(new MouseEvent("click")),e=void 0}export{a as s};
+//# sourceMappingURL=saveAs-D49p9Tqk.js.map
---
+++ assets/sheetList-B6QCIhlH.js
@@ -0,0 +1,2 @@
+import{B as e}from"./style-BzKcKm9k.js";const t=new e([]);export{t as s};
+//# sourceMappingURL=sheetList-B6QCIhlH.js.map
