3191b3ec968984773f92525f730481bc519673a7
Steve Xu <stevexugc@gmail.com>
2025-06-21 21:47:51 +0800

    feat: mono repo

--- packages/excel/jest.config.js
+++ packages/excel/jest.config.js
@@ -1,16 +1,17 @@
+const path = require('path');
+const scriptDir = path.join(__dirname, '../../scripts/');
 process.env.VITE_IS_E2E = 'true';
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  rootDir: process.cwd(),
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   testMatch: ['**/__tests__/**/*.test.ts?(x)'],
   transform: {
-    '^.+\\.css$': '<rootDir>/scripts/css-transform.js',
+    '^.+\\.css$': path.join(scriptDir, './css-transform.js'),
   },
   coverageReporters: ['clover', 'json', 'lcov', 'text', 'html'],
   moduleNameMapper: {
-    '\\.css$': '<rootDir>/scripts/css-mock.js',
+    '\\.css$': path.join(scriptDir, './css-mock.js'),
   },
-  setupFiles: ['<rootDir>/scripts/jest.setup.js'],
+  setupFiles: [path.join(scriptDir, './jest.setup.js')],
 };
