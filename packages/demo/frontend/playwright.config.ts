6451990fb48d01101a090b6c39ee98f2d8d6125e
Steve Xu <stevexugc@gmail.com>
2025-06-24 22:27:34 +0800

    fix: start server

--- packages/demo/frontend/playwright.config.ts
+++ packages/demo/frontend/playwright.config.ts
@@ -74,7 +74,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run dev:vite',
+    command: 'npm run start',
     url: 'http://127.0.0.1:3000',
     reuseExistingServer: true,
   },
