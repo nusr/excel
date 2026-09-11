80d5b476638629db26e82d11ad0f2142e6806622
Steve Xu <stevexugc@outlook.com>
2026-09-11 10:07:08 +0800

    fix: update sentry config

--- packages/demo/frontend/src/sentry.ts
+++ packages/demo/frontend/src/sentry.ts
@@ -1,12 +1,12 @@
-import * as Sentry from '@sentry/react';
+import * as Sentry from "@sentry/react";
 
 if (
-  typeof window !== 'undefined' &&
-  window.location.hostname === 'nusr.github.io' &&
+  typeof window !== "undefined" &&
+  window.location.hostname === "nusr.github.io" &&
   !Sentry.isInitialized()
 ) {
   Sentry.init({
-    dsn: 'https://b292d91ba509038c141ecfc7d10e7bb7@o4506851168092160.ingest.us.sentry.io/4506851171041280',
+    dsn: "https://b292d91ba509038c141ecfc7d10e7bb7@o4506851168092160.ingest.us.sentry.io/4506851171041280",
     integrations: [
       // If you're using react router, use the integration for your react router version instead.
       // Learn more at
@@ -15,7 +15,7 @@ if (
       Sentry.replayIntegration(),
       Sentry.feedbackIntegration({
         // Additional SDK configuration goes in here, for example:
-        colorScheme: 'system',
+        colorScheme: "system",
       }),
     ],
     // Set tracesSampleRate to 1.0 to capture 100%
@@ -31,5 +31,6 @@ if (
     // https://docs.sentry.io/platforms/javascript/session-replay/configuration/#general-integration-configuration
     replaysSessionSampleRate: 0.1,
     replaysOnErrorSampleRate: 1.0,
+    release: window.__bundle_info?.commit_id,
   });
 }
