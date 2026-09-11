bf50048ec4151877efda23b2c58e9291df0b9f6f
Steve Xu <stevexugc@outlook.com>
2026-09-11 09:57:42 +0800

    Update sentry.ts

--- packages/demo/frontend/src/sentry.ts
+++ packages/demo/frontend/src/sentry.ts
@@ -1,6 +1,10 @@
 import * as Sentry from '@sentry/react';
 
-if (location.hostname.includes('nusr.github.io')) {
+if (
+  typeof window !== 'undefined' &&
+  window.location.hostname === 'nusr.github.io' &&
+  !Sentry.isInitialized()
+) {
   Sentry.init({
     dsn: 'https://b292d91ba509038c141ecfc7d10e7bb7@o4506851168092160.ingest.us.sentry.io/4506851171041280',
     integrations: [
@@ -20,11 +24,7 @@ if (location.hostname.includes('nusr.github.io')) {
     // https://docs.sentry.io/platforms/javascript/configuration/options/#traces-sample-rate
     tracesSampleRate: 1.0,
     // Set `tracePropagationTargets` to control for which URLs trace propagation should be enabled
-    tracePropagationTargets: [
-      /^\//,
-      /^https:\/\/nusr.github.io\//,
-      'nusr.github.io',
-    ],
+    tracePropagationTargets: [/^https:\/\/nusr\.github\.io/],
     // Capture Replay for 10% of all sessions,
     // plus for 100% of sessions with an error
     // Learn more at
